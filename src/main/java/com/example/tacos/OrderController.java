92dd21faa4fad671c122786283710fd1e4a06809
Maksym <milian.code@gmail.com>
2022-11-05 15:27:55 +0100

    TacoCloud

---
+++ src/main/java/com/example/tacos/OrderController.java
@@ -0,0 +1,25 @@
+package com.example.tacos;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.support.SessionStatus;
+
+import javax.validation.Valid;
+
+@Slf4j
+@Controller
+public class OrderController {
+
+    @PostMapping
+    public String processOrder(@Valid TacoOrder order, Errors errors, SessionStatus sessionStatus) {
+        if (errors.hasErrors()) {
+            return "orderForm";
+        }
+
+        log.info("Order submitted: {} ", order);
+        sessionStatus.setComplete();
+        return "redirect:/";
+    }
+}
